8db67fbe317646c51e983ea63b000b734b6cc7a0
ducanhnguyen94 <ndanh4198@gmail.com>
2025-07-08 16:57:55 +0700

    BaiTap

---
+++ NgayHoc/IdeaProjects/LamQuen/05072025/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
