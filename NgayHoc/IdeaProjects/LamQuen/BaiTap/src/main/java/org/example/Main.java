b3792186ab0eafdbad22310ded3c97746144c8a1
ducanhnguyen94 <ndanh4198@gmail.com>
2025-07-08 16:53:32 +0700

    BaiTap

---
+++ NgayHoc/IdeaProjects/LamQuen/BaiTap/src/main/java/org/example/Main.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//          Bai1
+//        Scanner number1 = new Scanner(System.in);
+//        System.out.print("Nhap so thu nhat");
+//        int numberA = number1.nextInt();
+//
+//        Scanner number2 = new Scanner(System.in);
+//        System.out.print("Nhap so thu hai");
+//        int numberB = number2.nextInt();
+//
+//        if (numberA % numberB == 0) System.out.print("A chia het cho B");
+//        else System.out.print("A khong chia het cho B");
+
+//        Bai2
+//        Scanner inputNumber = new Scanner(System.in);
+//        System.out.print("Nhap tuoi cua hoc sinh");
+//        int age = inputNumber.nextInt();
+//
+//        if (age == 15) System.out.print("Hoc sinh du tuoi");
+//        else System.out.print("Hoc sinh khong du tuoi");
+//    }
+
+//        Bai3
+//        Scanner inputNumber = new Scanner(System.in);
+//        System.out.println("Nhap mot so");
+//        int number = inputNumber.nextInt();
+//
+//        if (number < 0) System.out.print("So nay nho hon 0");
+//        else if (number > 0) System.out.print("So nay lon hon 0");
+//        else System.out.print("So nay bang 0");
+
+//        Bai4
+//        Scanner inputNumber1 = new Scanner(System.in);
+//        System.out.println("Nhap so thu nhat");
+//        int number1 = inputNumber1.nextInt();
+//        Scanner inputNumber2 = new Scanner(System.in);
+//        System.out.println("Nhap so thu hai");
+//        int number2 = inputNumber1.nextInt();
+//        Scanner inputNumber3 = new Scanner(System.in);
+//        System.out.println("Nhap so thu ba");
+//        int number3 = inputNumber1.nextInt();
+//        if (number1 > number2) {
+//            if (number1 > number3) System.out.print(number1 + " la so lon nhat");
+//            else System.out.print(number3 + " la so lon nhat");
+//        } else {
+//            if (number2 > number3) System.out.print(number2 + " la so lon nhat");
+//            else System.out.print(number3 + " la so lon nhat");
+//        };
+
+//        Bai5
+//        Scanner inputNumber1 = new Scanner(System.in);
+//        System.out.println("Nhap so thu nhat");
+//        int number1 = inputNumber1.nextInt();
+//        Scanner inputNumber2 = new Scanner(System.in);
+//        System.out.println("Nhap so thu hai");
+//        int number2 = inputNumber1.nextInt();
+//        Scanner inputNumber3 = new Scanner(System.in);
+//        System.out.println("Nhap so thu ba");
+//        int number3 = inputNumber1.nextInt();
+//        double TB = (number1 + number2 + number3) / 3;
+//        if (TB >= 8.5) System.out.print("Hoc sinh gioi");
+//        else if (TB >= 7.5) System.out.print("Hoc sinh kha");
+//        else if (TB >=6.5) System.out.print("hoc sinh TB");
+//        else System.out.print("Hoc sinh kem");
+
+//        Bai6
+        int number;
+        Scanner inputNumber = new Scanner(System.in);
+        do {
+            System.out.println("Nhap mot so");
+            number = inputNumber.nextInt();
+        } while (number != 0);
+    }
+}
