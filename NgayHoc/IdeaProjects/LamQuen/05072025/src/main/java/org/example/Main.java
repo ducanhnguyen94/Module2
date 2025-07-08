aa1e9c68805def614fdfc34b59281ef7f2a8dfad
ducanhnguyen94 <ndanh4198@gmail.com>
2025-07-08 17:34:01 +0700

    BaiTap

--- NgayHoc/IdeaProjects/LamQuen/05072025/src/main/java/org/example/Main.java
+++ NgayHoc/IdeaProjects/LamQuen/05072025/src/main/java/org/example/Main.java
@@ -1,28 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+//
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j < i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+//
+//        for (int i = 5; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j < i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.println("Nhap mot so nguyen bat ky");
+        int numbers = inputNumber.nextInt();
+        int count = 0;
+        int N = 2;
 
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        while (count < numbers) {
+            int uoc = 0;
+            for (int i = 1; i <= N; i++) {
+                if (N % i == 0) uoc++;
             }
-            System.out.println("");
+            if (uoc == 2) {
+                System.out.println(N);
+                N++;
+                count++;
+            } else N++;
         }
     }
 }
